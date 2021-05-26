a0be2e4dde69242acc9f2b7083d2fd2a63268ad7
Administrator <admin@example.com>
2021-05-26 19:02:24 +0530

    cli test case for api test

--- cli_testing/Run.sh
+++ cli_testing/Run.sh
@@ -1,2 +1,2 @@
-python3 -m pytest -s  cli_verification.py  --excelname reantest_testcases.xlsx --alluredir=/home/hcaptest/hcap-test/code/cli_testing/uireports 
+python3 -m pytest -s  cli_verification.py  --excelname testcases.xlsx --alluredir=/home/hcaptest/hcap-test/code/cli_testing/uireports 
 cd /home/hcaptest/hcap-test/code/cli_testing/uireports
